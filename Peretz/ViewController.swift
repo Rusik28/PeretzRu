b3f174122e863f57b4f8185b325df85dbfec5150
Ruslan ALiev <rusik_aliev_2002@mail.ru>
2022-02-08 18:57:59 +0300

    Initial Commit

---
+++ Peretz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Peretz
+//
+//  Created by Руслан Алиев on 08.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
